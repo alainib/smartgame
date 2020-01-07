bd68bc6ecc0bb71b0f7ffa9b2403dd0cd4f6a582
alain <ibrahimalain@hotmail.com>
2020-01-08 00:48:31 +0100

    ajout brick restantes en cour

--- src/components/Partie.js
+++ src/components/Partie.js
@@ -2,19 +2,12 @@ import React, { Component } from "react";
 import { StyleSheet, css } from "aphrodite";
 
 import "App.css";
-function clj(data) {
-  console.log(JSON.stringify(data, undefined, 2));
-}
-function clnj(astring, data) {
-  console.log(astring);
-  console.log(JSON.stringify(data, undefined, 2));
-  console.log("---------------");
-}
 
-const _supportSize = {
+const _baseSize = {
   x: 11,
   y: 5
 };
+
 const _bricks = {
   a: {
     color: "orange",
@@ -119,8 +112,9 @@ export default class Partie extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      supportData: this.initSupport(),
+      baseData: this.initBase(),
       colors: this.initColors(),
+      usedLetters: [],
       bricks: _bricks
     };
   }
@@ -131,29 +125,31 @@ export default class Partie extends Component {
     }
     return colors;
   }
-  initSupport() {
-    // const res = [...Array(_supportSize.y)].map(x => Array(_supportSize.x).fill(false));
-    let res = [
+
+  initBase() {
+    const res = [...Array(_baseSize.y)].map(x => Array(_baseSize.x).fill(false));
+    /* const res = [
       ["a", "a", "a", "a", false, false, "i", false, false, false, false],
       ["b", "b", "b", false, false, false, "i", false, false, false, false],
       ["b", false, false, false, false, false, "i", false, false, false, false],
       [false, false, false, false, false, false, "i", false, false, false, false],
       [false, "j", "j", "j", "j", "j", false, false, false, false, false]
-    ];
+    ];*/
     return res;
   }
 
-  async componentDidMount() {
+  componentDidMount = async () => {
     this.trySetBrickAt("l", 0, 3);
     this.trySetBrickAt("i", 2, 1);
-  }
+    this.trySetBrickAt("a", 2, 9);
+  };
 
   /** 
    * essai de mettre une brick dans une position
    @param name of the brick:a b c ....
    @param x abscisse index to set the brick at
    @param y ordonate index to set the brick at
-   on represente le support comme un support comme matrix également
+   on represente le base comme une matrix également
   
    0 _ _ _ _ _ _ _ > x 
    | 
@@ -164,70 +160,70 @@ export default class Partie extends Component {
   
   
   */
-  trySetBrickAt(name, x, y) {
-    let { supportData } = this.state;
-
+  trySetBrickAt = (name, x, y) => {
     const can = this.canSetBrick(name, x, y);
 
     if (can) {
       this.setBrickAt(name, x, y);
-      this.setState({ supportData });
     } else {
       console.log("cannot be set here ! ");
     }
-  }
+  };
 
   // met la brick dans cette position
-  setBrickAt(name, x, y) {
+  setBrickAt = (name, x, y) => {
     const brick = this.state.bricks[name];
-    console.log(brick);
-    const bsize = {
+
+    const brickSize = {
       x: this.getXLength(brick.matrix),
       y: this.getYLength(brick.matrix)
     };
-    let supportData = [...this.state.supportData];
-    for (var i = 0; i < bsize.x; i++) {
-      for (var j = 0; j < bsize.y; j++) {
+    let baseData = [...this.state.baseData];
+    for (var i = 0; i < brickSize.x; i++) {
+      for (var j = 0; j < brickSize.y; j++) {
         if (brick.matrix[i][j] === 1) {
-          supportData[i + x][j + y] = name;
+          baseData[i + x][j + y] = name;
         }
       }
     }
-    console.log(supportData);
-    this.setState({ supportData });
-  }
+
+    this.setState((prevState, props) => ({
+      usedLetters: [...prevState.usedLetters, name],
+      baseData
+    }));
+  };
 
   // check si on peut mettre la brick à la position (x,y). il faut que la place soit bien vide
-  canSetBrick(name, x, y) {
-    let { supportData } = this.state;
+  canSetBrick = (name, x, y) => {
+    let { baseData } = this.state;
     const brick = this.state.bricks[name];
     let can = true;
-    // on test que la brick ne depasse pas du support
+    // on test que la brick ne depasse pas du base
 
-    const bsize = {
+    const brickSize = {
       x: this.getXLength(brick.matrix),
       y: this.getYLength(brick.matrix)
     };
-    const supportSize = {
-      x: this.getXLength(supportData),
-      y: this.getYLength(supportData)
+    const baseSize = {
+      x: this.getXLength(baseData),
+      y: this.getYLength(baseData)
     };
 
-    // console.log({ x, "bsize.x": bsize.x, "supportSize.x": supportSize.x, res: x + bsize.x > supportSize.x });
-    if (x + bsize.x > supportSize.x) {
+    if (x + brickSize.x > baseSize.x) {
+      console.log({ x, "brickSize.x": brickSize.x, "baseSize.x": baseSize.x, res: x + brickSize.x > baseSize.x });
       return false;
     }
 
-    // console.log({ y, "bsize.y": bsize.y, "supportSize.y": supportSize.y, res: y + bsize.y > supportSize.y });
-    if (y + bsize.y > supportSize.y) {
+    if (y + brickSize.y > baseSize.y) {
+      console.log({ y, "brickSize.y": brickSize.y, "baseSize.y": baseSize.y, res: y + brickSize.y > baseSize.y });
       return false;
     }
 
-    // on crop une sous matrix de taille identique à brick dans supportData, plus facile pour comparer
-    const sub = this.getSub(supportData, x, y, bsize.x, bsize.y);
-    console.log(sub, brick.matrix);
-    for (var i = 0; i < bsize.x; i++) {
-      for (var j = 0; j < bsize.y; j++) {
+    // on crop une sous matrix de taille identique à brick dans baseData, plus facile pour comparer
+    const sub = this.getSub(baseData, x, y, brickSize.x, brickSize.y);
+    // console.log(sub, brick.matrix);
+    for (var i = 0; i < brickSize.x; i++) {
+      for (var j = 0; j < brickSize.y; j++) {
         if (brick.matrix[i][j] === 1) {
           if (sub[i][j] !== false) {
             can = false;
@@ -237,7 +233,7 @@ export default class Partie extends Component {
     }
 
     return can;
-  }
+  };
   /**
    * découpe une sous matrice dans brick
    * @param {*} brick
@@ -257,20 +253,21 @@ export default class Partie extends Component {
     }
     return max;
   }
+
   getXLength(brick) {
     return brick.length;
   }
 
-  renderSupport() {
-    const { supportData, colors } = this.state;
+  renderBase() {
+    const { baseData, colors } = this.state;
 
     return (
       <div>
-        {supportData.map((row, i) => (
+        {baseData.map((row, i) => (
           <div className={css(styles.bloc)} key={i}>
             {row.map((col, j) => {
               return (
-                <div className={css(styles.case)} style={{ backgroundColor: colors[col] }} key={j}>
+                <div className={css(styles.item)} style={{ backgroundColor: colors[col] }} key={j}>
                   <span className={css(styles.smalltitle)}>{col + i + "," + j}</span>
                 </div>
               );
@@ -280,13 +277,50 @@ export default class Partie extends Component {
       </div>
     );
   }
+  renderUnusedBricks() {
+    let show = [];
+    for (var i in _bricks) {
+      if (!this.state.usedLetters.includes(i)) {
+        show.push(
+          <div key={i}>
+            <ShowBrick name={i} data={_bricks[i]} />
+          </div>
+        );
+      }
+    }
+    return <div className="flex-container wrap">{show}</div>;
+  }
 
   render() {
     return (
       <div>
-        <h2 className="padding15">Partie :</h2>
+        <h2>Partie :</h2>
         <br />
-        {this.renderSupport()}
+
+        {this.renderBase()}
+
+        {this.renderUnusedBricks()}
+      </div>
+    );
+  }
+}
+
+class ShowBrick extends Component {
+  render() {
+    const { matrix, color } = this.props.data;
+
+    return (
+      <div key={this.props.name} className="margin10">
+        <div>{this.props.name}</div>
+        {matrix.map((row, i) => {
+          return (
+            <div className={css(styles.bloc)} key={i}>
+              {row.map((col, j) => {
+                return <div className={css(styles.itemSmall)} style={{ backgroundColor: col ? color : false }} key={j}></div>;
+              })}
+            </div>
+          );
+        })}
       </div>
     );
   }
@@ -297,20 +331,21 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between"
+
+    justifyContent: "center"
   },
+
   item: {
-    width: 175,
-    height: 75,
-    margin: 5
+    width: 80,
+    height: 80
+  },
+  itemSmall: {
+    width: 40,
+    height: 40
   },
   smalltitle: {
     fontSize: 15,
     color: "white"
-  },
-  case: {
-    width: "100px",
-    height: "100px"
   }
 });
 
